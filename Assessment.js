9f8172401faf703cdc7e50753c3060a8d39ff360
Andrewk311 <Andrewking0619@gmail.com>
2021-04-28 00:38:26 -0400

    Used for loops to traverse each digit and print out the words

--- Assessment.js
+++ Assessment.js
@@ -1,3 +1,7 @@
+function getlength(number) { 
+    return number.toString().length; 
+}
+
 const inputs = new Array(process.argv.length-2);
 let words = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
 const outputs = new Array(process.argv.length-2);
@@ -5,4 +9,21 @@ const outputs = new Array(process.argv.length-2);
 for (let i = 2; i < process.argv.length; i++){  
     inputs[i-2] = process.argv[i];
 }
+
+let result = '';
+
+for (let i = 0; i < inputs.length; i++){
+    let x = inputs[i];
+    const digits = new Array(getlength(x));
+    for (let j = getlength(x) - 1; j >= 0; j--){
+        digits[j] = words[x%10];
+        x = Math.floor(x/10);
+    }                                                    
+    for (let k = 0; k < digits.length; k++){
+        result += digits[k];
+    }
+    result += ',';                                             
+}
+
+console.log(result.substring(0,result.length-1));
 
